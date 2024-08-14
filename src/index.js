5c55609b0c7a16eee075366dd1f5de06de3680e8
thalys m cotta <thalysm.cotta@gmail.com>
2024-08-14 18:19:35 -0300

    fk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const app= express();
+const path = require('path');
 
+const app= express();
+/*
+//EJS Configuration
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//Static File Configuration
+app.use(express.static('public'))
+
+
+//Reading Requisition Configuration
+app.use(express.urlencoded({ extended: true}))*/
 
 const PORT = process.env.PORT || 3000;
 
